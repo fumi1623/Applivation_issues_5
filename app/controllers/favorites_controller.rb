a8eb247f8167198f0c7515ab1f14866bbe4b610e
EC2 Default User <ec2-user@ip-172-31-25-148.ec2.internal>
2021-05-03 11:59:03 +0000

    [comment&favorite]Asynchronous communication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id) #←非同期通信用の記述
+    # favorite = current_user.favorites.new(book_id: book.id) #元の記述
     favorite.save
-    redirect_back fallback_location: @book
+    # redirect_back fallback_location: @book
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id) #←非同期通信用の記述
+    # favorite = current_user.favorites.find_by(book_id: book.id)  #元の記述
     favorite.destroy
-    redirect_back fallback_location: @book
+    # redirect_back fallback_location: @book
   end
 
 end
